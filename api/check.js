cc96dceff61c1311105fdcd141daefda0e836296
Wellington Andrade <web3explorer01@email.com>
2026-06-06 17:16:42 -0300

    Testando variaveis da Vercel

--- api/check.js
+++ api/check.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   return res.status(200).json({
+    testeExiste: !!process.env.TESTE_ENV,
     supabaseUrlExiste: !!process.env.SUPABASE_URL,
     supabaseKeyExiste: !!process.env.SUPABASE_SERVICE_KEY
   });
